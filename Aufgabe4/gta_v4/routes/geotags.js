493f3a21edcdfa8d0cda80dec9bf71f065f77fea
Hannes Schwarzer <hannesschwarzer@email.com>
2020-06-28 15:45:04 +0200

    Task 4: Put "geotags.js" into "gta-server.js"

--- Aufgabe4/gta_v4/routes/geotags.js
+++ Aufgabe4/gta_v4/routes/geotags.js
@@ -8,76 +8,7 @@ var jsonParser = bodyParser.json();
 const modul = require('inMemorySpeicherung');
 
 
-router.post('/', jsonParser, (req, res) => {
-    var searchArray = modul.radiusSearch(req.body.latitude, req.body.longitude);
-    var object = modul.addGeotag(
-        req.body.name, req.body.latitude, req.body.longitude, req.body.hashtag);
-    searchArray.push(object)
-    res.status(201)
-    res.location("/geotags/" + object.geotagID)
-    res.json(searchArray)
-})
 
-router.get('/', (req, res) => {
-    var searchResults = [];
-
-    var searchtermURL = req.query.searchterm;
-    var latitudeURL = req.query.latitude;
-    var longitudeURL = req.query.longitude;
-    var radius = req.query.radius;
-
-
-    if (searchtermURL) {
-        searchResults = modul.searchForGeotag(searchtermURL);
-    } else if (latitudeURL && longitudeURL && radius) {
-        searchResults = modul.radiusSearch(radius, latitudeURL, longitudeURL);
-    } else {
-        searchResults = modul.returnGeoTags()
-    }
-
-    res.JSON(searchResults)
-})
-
-
-/**
- * TODO: This is all required:
- use the functions established in gta-server.js
- router.post (/)
- router.get (/)
- router.xy (geotags/geotagsID): get, change, delete
- geotagsID has to be programmed (see example down below)
- use the functions established in gta-sever.js */
-
-router.delete('/:geotagID', (req, res) => {
-    var geotagID = req.params.geotagID;
-    var geoTags = modul.deleteGeotag(geotagID)
-    res.status(200)
-    console.log("Object" + geotagID + " deleted!")
-    res.json({
-        taglist: geoTags
-    });
-
-})
-
-router.get('/:geotagID', (req, res) => {
-    var geotagID = req.params.geotagID;
-    res.json({
-        taglist: modul.searchForGeotagID(geotagID)
-
-    })
-})
-
-router.put('/:geotagID', (req, res) => {
-    var geotagID = req.params.geotagID;
-    var latitudeURL = req.query.latitude;
-    var longitudeURL = req.query.longitude;
-    var nameURL = req.query.name;
-    var hashTag = req.query.hashTag;
-
-    modul.changeGeotag(nameURL, longitudeURL, latitudeURL, hashTag, geotagID);
-    res.status(200);
-    res.send();
-})
 
 module.exports = router;
 
